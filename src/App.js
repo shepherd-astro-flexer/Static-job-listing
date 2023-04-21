f85fc031de018818e047153a087f6e19182aeb82
astro-flex <flezo19@gmail.com>
2023-04-21 10:45:41 +0800

    Add chips

--- src/App.js
+++ src/App.js
@@ -1,30 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import data from "./data.json"; // Ohh, so we need to import it also
 
 function App() {
-  // const jsonData = JSON.parse(data);
+   const [search, setState] = useState("");
+   const [chip, setChip] = useState(["Javascript", "React"]);
+
+   const updateChip = (e) => {
+    const value = e.target.value;
+
+    if (e.key === "Enter") {
+      setChip(prevValue => {
+        return [...prevValue, value];
+      });
+      e.target.value = "";
+    }
+  }
 
   return (
     <div>
-      <header></header>
-      {data.map((item, index) => {
+      <header>
+        <div className="tags-input">
+          <ul>
+            {chip.map((item, index) => {
+              return (
+                <li key={index}>
+                  <span className="chip-content">{item}</span>
+                  <span onClick={() => {
+                    setChip(prevValue => {
+                      return prevValue.filter((item, idx) => idx !== index)
+                    })
+                  }} className="remove-chip">&#10006;</span>
+                </li>
+              )
+            })}
+          </ul>
+          <input type="text" onKeyUp={updateChip} placeholder="Press enter to add languages" />
+          <p onClick={() => {
+            setChip([]);
+          }} className="clear-button">Clear</p>
+        </div>
+      </header>
+      {data.filter(item => {
+        const lowerCaseLanguage = item.languages.map(language => language.toLowerCase());
+        return search.toLowerCase() === "" ? item : lowerCaseLanguage.some(language => language.includes(search.toLowerCase()));
+      }).map((item, index) => {
         return (
-          <div className="card">
+          <div className={`card ${item.featured && "featured-border"}`}>
             <div className="image-container">
               <img className="logo" src={item.logo} alt="" />
             </div>
             <div className="qualifications-container">
-              <div>
-                <h5>{item.company}</h5>
-                <h6 className={item.new ? "new condition" : ""}>{item.new && "NEW!"}</h6>
-                <h6 className={item.featured ? "featured condition" : ""}>{item.featured && "FEATURED"}</h6>
+              <div className="title-container">
+                <h5 className="title">{item.company}</h5>
+                <h6 className={item.new ? "new condition title" : ""}>{item.new && "NEW!"}</h6>
+                <h6 className={item.featured ? "featured condition title" : ""}>{item.featured && "FEATURED"}</h6>
               </div>
               <div>
                 <h4>{item.position}</h4>
               </div>
               <div className="type-container">
-                <p className="type">{item.postedAt} &middot;</p>
-                <p className="type">{item.contract} &middot;</p>
+                <p className="type">{item.postedAt}<span className="bullet">&bull;</span></p>
+                <p className="type">{item.contract}<span className="bullet">&bull;</span></p>
                 <p className="type">{item.location}</p>
               </div>
             </div>
@@ -45,5 +81,40 @@ function App() {
     </div>
   );
 }
+
+// item.languages.filter(language => {
+//   return search.toLowerCase() === "" ? language : language.toLowerCase().includes(search);
+// }
+// ! start
+{/* <input onChange={(e) => {
+          const value = e.target.value;
+          console.log(value)
+          setState(value);
+        }} value={search} name="search" type="text" /> */}
+// ! end
+{/* <header>
+<input value={search} onChange={(e) => {
+  const value = e.target.value;
+
+  setSearch(value);
+}} type="text" />
+</header>
+<div>
+{data.filter(item => {
+  {/* return item.languages.filter(languages => {
+    return search.toLowerCase() === "" ? languages : languages.toLowerCase().includes(search);
+  }) */}
+//   return search.toLowerCase() === "" ? item : item.role.toLowerCase().includes(search);
+// })
+// .map(item => {
+//   return (
+//     <div>
+//       <img src={item.logo} alt="" />
+//       <p>{item.role}</p>
+//       <p>{item.level}</p>
+//     </div>
+//   )
+// })}
+// </div> */}
 
 export default App;
